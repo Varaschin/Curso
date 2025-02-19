92b69c2582b2752dd270fe6cc9119146d88b13fe
Varaschin <45366865+Varaschin@users.noreply.github.com>
2025-02-19 20:15:52 -0300

    Update supert_trunfo.c

--- supert_trunfo.c
+++ supert_trunfo.c
@@ -1,17 +1,18 @@
-// Desafio Novato Super Trunfo
+// Curso Análise e Desenvolvimento de Sistemas
+// Aluno: Rafael Varaschin
+// Matrícula: 202501605966 - Polo Centro Curitiba/Pr.
+// Tema 1 - Super Trunfo em c: Fundamentos e Técnicas Avançadas
 
 #include <stdio.h>
 
 int main () {
 
 //Declaração das variáveis da Carta 1
-
         char estado [1], codigo_carta [4], cidade [50];
         int populacao, pontos_turisticos;
         float area, pib;
 
 // Declaração das variáveis da Carta 2
-
         char estado2 [1], codigo_carta2 [4], cidade2 [50];
         int populacao2, pontos_turisticos2;
         float area2, pib2;
@@ -20,11 +21,11 @@ int main () {
 // Inserindo dados da Carta 1:
         printf ("CADASTRO DA CARTA 1 \n\n");
 
-        printf("A = Sao Paulo \nB = Rio de Janeiro \n");
+        printf("A = Sao Paulo \nB = Rio de Janeiro \nC = Parana \nD = Santa Catarina \nE = Rio Grande do Sul \nF = Pernambuco \nG = Ceara \nH = Rio Grande do Norte \n\n");
         printf("Digite a Inicial do Estado: ");
         scanf ("%s", &estado);
 
-        printf("Codigo da Carta.\nDevera ser a inicial do Estado, seguida de um nr de 2 digitos, exemplo (A01, B03)\n");
+        printf("Codigo da Carta.\nDevera ser a inicial do Estado, seguida de 2 digitos, exemplo (A01, B03)\n");
         printf("Digite o Codigo da Carta: ");
         scanf("%s", &codigo_carta);
 
@@ -48,11 +49,10 @@ int main () {
 // Inserindo dados da Carta 2:
         printf ("CADASTRO DA CARTA 2 \n\n");
 
-        printf("A = Sao Paulo \nB = Rio de Janeiro \n");
         printf("Digite a Inicial do Estado: ");
         scanf ("%s", &estado2);
 
-        printf("Codigo da Carta.\nDevera ser a inicial do Estado, seguida de 2 digitos, exemplo (A01, B03)\n");
+        printf("Codigo da Carta.\nDevera ser a inicial do Estado, seguida de um nr de 2 digitos, exemplo (A01, B03)\n");
         printf("Digite o Codigo da Carta: ");
         scanf("%s", &codigo_carta2);
 
@@ -74,32 +74,28 @@ int main () {
         printf("--------------------\n");
 
 // Imprimindo dados da Carta 1:
-
         printf("CARTA 1:\n");
         printf("Estado: %s\n", estado);
         printf("Codigo da Carta: %s\n", codigo_carta);
         printf("Nome da Cidade: %s\n", cidade);
         printf("Populacao: %i\n", populacao);
-        printf("Area em Km2: %.2f\n", area);
-        printf("PIB em Bilhoes: %.2f\n", pib);
+        printf("Area %.2f", area); printf (" km2\n");
+        printf("PIB: %.2f", pib); printf (" Bilhoes de Reais\n");
         printf("Nr de Pontos Turisticos: %i\n", pontos_turisticos);
 
         printf("--------------------\n");
 
 // Imprimindo dados da Carta 2:
-
         printf("CARTA 2:\n");
         printf("Estado: %s\n", estado2);
         printf("Codigo da Carta: %s\n", codigo_carta2);
         printf("Nome da Cidade: %s\n", cidade2);
         printf("Populacao: %i\n", populacao2);
-        printf("Area em Km2: %.2f\n", area2);
-        printf("PIB em Bilhoes: %.2f\n", pib2);
+        printf("Area: %.2f", area2);  printf (" km2\n");
+        printf("PIB: %.2f", pib2); printf (" Bilhoes de Reais\n");
         printf("Nr de Pontos Turisticos: %i\n", pontos_turisticos2);
 
         printf("--------------------\n");
 
-        
         return 0;
-
 }
